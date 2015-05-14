71643434370e7d24dd8b6282a15ec87993659949
oli <jose@localhost.localdomain>
2015-05-14 14:36:28 +0200

    fix: username may contain space

--- upload2osm/osmtrackupload.sh
+++ upload2osm/osmtrackupload.sh
@@ -9,7 +9,7 @@ shift
 for i
         do
         echo $i
-curl -u ${user}:${password} -H "Expect: " -F "file=@$i" -F description="${description}" \
+curl -u "${user}:${password}" -H "Expect: " -F "file=@$i" -F description="${description}" \
    -F tags=${tags} -F visibility=identifiable http://www.openstreetmap.org/api/0.6/gpx/create
 done
 
