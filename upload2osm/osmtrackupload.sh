e38169868f086a828e73b83eac4865104cb54851
jose1711 <jose1711@gmail.com>
2016-02-07 22:41:36 +0100

    there's probably no reason *not* to go with https

--- upload2osm/osmtrackupload.sh
+++ upload2osm/osmtrackupload.sh
@@ -10,7 +10,7 @@ for i
         do
         echo $i
 curl -u "${user}:${password}" -H "Expect: " -F "file=@$i" -F description="${description}" \
-   -F tags=${tags} -F visibility=identifiable http://www.openstreetmap.org/api/0.6/gpx/create
+   -F tags=${tags} -F visibility=identifiable https://www.openstreetmap.org/api/0.6/gpx/create
 done
 
 exit 0
