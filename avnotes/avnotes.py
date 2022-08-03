163e8e83cd805057e0a363abf722a3305d145f63
Jose Riha <jose1711@gmail.com>
2022-08-03 08:09:44 +0200

    avnotes.py: Migrate to py3

--- avnotes/avnotes.py
+++ avnotes/avnotes.py
@@ -1,13 +1,13 @@
-#!/usr/bin/python2
+#!/usr/bin/python3
 import math
 import sys
 import os
 import re
-import datetime
+from datetime import datetime
 import argparse
 
 # ARRAY = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_~'
-ARRAY = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_@'
+ARRAY = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_@"
 
 
 def string2geo(s):
@@ -28,7 +28,7 @@ def string2geo(s):
         code = (code << 6) | digit
 
     # align to 64bit integer
-    code = (code << (62 - (6 * len(s))))
+    code = code << (62 - (6 * len(s)))
     x = y = 0
 
     # deinterleaving
@@ -36,23 +36,24 @@ def string2geo(s):
         x = (x << 1) | ((code >> i) & 1)
         y = (y << 1) | ((code >> (i - 1)) & 1)
 
-    lat = (y << 1) / (2 ** 32 / 180.0) - 90
-    lon = (x << 1) / (2 ** 32 / 360.0) - 180
+    lat = (y << 1) / (2**32 / 180.0) - 90
+    lon = (x << 1) / (2**32 / 360.0) - 180
 
     return (lat, lon)
 
+
 aparser = argparse.ArgumentParser()
-aparser.add_argument('-s', nargs=1, help='Only convert STRING and output to stdout', metavar='STRING')
+aparser.add_argument("-s", nargs=1, help="Only convert STRING and output to stdout", metavar="STRING")
 args = aparser.parse_args()
 
 if args.s:
-    print string2geo(args.s[0])
+    print(string2geo(args.s[0]))
     sys.exit()
 
 # process files in current directory
 files = []
 waypoints = []
-for (dirpath, dirnames, filenames) in os.walk('.'):
+for (dirpath, dirnames, filenames) in os.walk("."):
     files.extend(filenames)
     break
 
@@ -62,16 +63,16 @@ files.sort(key=lambda x: os.stat(x).st_mtime)
 c = 0
 
 # grep for files matching 3gp extension
-audiofiles = filter(lambda x: re.search(r'\.3gp$', x), files)
+audiofiles = [x for x in files if re.search(r"\.3gp$", x)]
 
 if not audiofiles:
     sys.exit(0)
 
-print "<?xml version='1.0' encoding='UTF-8'?>"
-print "<gpx version='1.1' creator='osmand2gpx.py' xmlns='http://www.topografix.com/GPX/1/1'>"
+print("<?xml version='1.0' encoding='UTF-8'?>")
+print("<gpx version='1.1' creator='osmand2gpx.py' xmlns='http://www.topografix.com/GPX/1/1'>")
 
 
-for string in map(lambda x: re.sub("(.*)\.3gp", r"\1", x), audiofiles):
+for string in [re.sub("(.*)\.3gp", r"\1", x) for x in audiofiles]:
     basename = string
     # string=re.sub("-","",string)
     string = re.sub("-.*", "", string)
@@ -79,9 +80,9 @@ for string in map(lambda x: re.sub("(.*)\.3gp", r"\1", x), audiofiles):
 
     c += 1
 
-    os.system('ffmpeg -y -i ' + basename + '.3gp ' + basename + '.3gp.wav')
-    times = (os.stat(basename + '.3gp').st_mtime, os.stat(basename + '.3gp').st_mtime)
-    os.utime(basename + '.3gp.wav', times)
+    os.system("ffmpeg -y -i " + basename + ".3gp " + basename + ".3gp.wav")
+    times = (os.stat(basename + ".3gp").st_mtime, os.stat(basename + ".3gp").st_mtime)
+    os.utime(basename + ".3gp.wav", times)
     waypoints.append([lon, lat, basename, c, times[0]])
 
 if len(waypoints) < 1:
@@ -93,13 +94,13 @@ for wpt in waypoints:
     basename = wpt[2]
     name = wpt[3]
     time = wpt[4]
-    print "<wpt lon='" + repr(lon) + "' lat='" + repr(lat) + "'>"
-    print "<time>" + str(datetime.datetime.fromtimestamp(time)).replace(' ', 'T') + 'Z' + "</time>"
-    print "<name>" + repr(name) + "</name>"
-    print "<link href='" + basename + ".3gp.wav'/>"
-    print "</wpt>"
+    print("<wpt lon='" + repr(lon) + "' lat='" + repr(lat) + "'>")
+    print("<time>" + str(datetime.fromtimestamp(time)).replace(" ", "T") + "Z" + "</time>")
+    print("<name>" + repr(name) + "</name>")
+    print("<link href='" + basename + ".3gp.wav'/>")
+    print("</wpt>")
 
-print "<trk><trkseg>"
+print("<trk><trkseg>")
 
 for wpt in waypoints:
     lon = wpt[0]
@@ -107,9 +108,9 @@ for wpt in waypoints:
     basename = wpt[2]
     name = wpt[3]
     time = wpt[4]
-    print "<trkpt lon='" + repr(lon) + "' lat='" + repr(lat) + "'>"
-    print "<time>" + str(datetime.datetime.fromtimestamp(time)).replace(' ', 'T') + 'Z' + "</time>"
-    print "</trkpt>"
+    print("<trkpt lon='" + repr(lon) + "' lat='" + repr(lat) + "'>")
+    print("<time>" + str(datetime.fromtimestamp(time)).replace(" ", "T") + "Z" + "</time>")
+    print("</trkpt>")
 
-print "</trkseg></trk>"
-print "</gpx>"
+print("</trkseg></trk>")
+print("</gpx>")
